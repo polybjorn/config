5e176c9d5bc5e89eb7633c909638cbe6c19ebedd
polybjorn <bjanda@protonmail.com>
2022-09-06 20:20:51 +0200

    name correction

---
+++ python/pythonstartup.py
@@ -0,0 +1,15 @@
+# ${XDG_CONFIG_HOME}/python/pythonstartup.py
+
+import atexit
+import os
+import readline
+
+histfile = os.path.join(os.getenv("XDG_CACHE_HOME", os.path.expanduser("~/.cache")), ".python_history")
+try:
+    readline.read_history_file(histfile)
+    # default history len is -1 (infinite), which may grow unruly
+    readline.set_history_length(1000)
+except FileNotFoundError:
+    pass
+
+atexit.register(readline.write_history_file, histfile)
